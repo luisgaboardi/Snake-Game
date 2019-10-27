21de5b75d8e65b02d8d511c679466f4120051194
Luís Guilherme <luis.ggaboardi@gmail.com>
2019-10-27 19:55:39 +0000

    Added Main.java file

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+package visual;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela window = new Tela();
+					window.getJanelaJogo().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
