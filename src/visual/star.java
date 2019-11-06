8e93cfec87bdfa51afea7ace00927fed6e83de60
Liverson Paulo <liverson.p@gmail.com>
2019-11-05 22:49:29 -0300

    Created the type of snake star, that can receive double of points than the normal one

---
+++ src/visual/star.java
@@ -0,0 +1,11 @@
+package visual;
+
+import java.awt.*;
+
+public class star extends snake {
+    public star(int Pontos) {
+        super(Pontos);
+        cor = new Color(255,255,51);
+        score = 0;
+    }
+}
