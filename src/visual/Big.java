e45ae666331859eed2bd5b4daafad9de8579924d
Liverson Paulo <liverson.p@gmail.com>
2019-11-10 19:08:45 -0300

    Created the fruit Big, this fruit will increase the score 2 times, it's color is orange

--- src/visual/Big.java
+++ src/visual/Big.java
@@ -1,4 +1,11 @@
 package visual;
 
-public class Big {
+import java.awt.*;
+
+public class Big extends Fruta {
+
+    private static Color cor1;
+    public Big(){
+        setCor2(new Color(255,255,102));
+    }
 }
