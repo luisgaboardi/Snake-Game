0b38a724ec7b68f1d9c29756a81792eed6c66340
Liverson Paulo <liverson.p@gmail.com>
2019-11-10 21:20:23 -0300

    This new class is to create obstacles in the board, it has the construtor and gerarObstaculo, the method gerarObstaculo create a obstacle in a random position

--- src/visual/Obstaculo.java
+++ src/visual/Obstaculo.java
@@ -1,4 +1,36 @@
 package visual;
 
+import java.awt.*;
+
 public class Obstaculo {
+    private int escala = 10;
+
+    private int posX;
+    private int posY;
+    private static Color cor;
+
+    public Obstaculo(){
+        cor = new Color(255,255,255);
+        gerarObstaculo();
+    }
+
+    public void gerarObstaculo(){
+        int r = (int) (Math.random() * 37);
+        posX = r*escala;
+
+        r = (int) (Math.random() * 28);
+        posY = r*escala;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public static Color getCor() {
+        return cor;
+    }
 }
