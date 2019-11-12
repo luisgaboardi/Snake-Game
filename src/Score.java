034c5cf2163eee09681b8319e45b4f9fa267ec43
Liverson Paulo <liverson.p@gmail.com>
2019-11-11 23:57:54 -0300

    This class will create a file with the scores and take the high score to print in the screen

--- src/Score.java
+++ src/Score.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.IOException;
 
 public class Score {
+    public Score(){
+        File file = new File("Scores");
+        file.mkdir();
+
+        File txt = new File("Scores/Scores.txt");
+        try {
+            txt.createNewFile();
+        }catch (IOException ex){
+
+        }
+    }
 }
