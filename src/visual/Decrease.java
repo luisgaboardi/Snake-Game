4219e7a34254056865c8c627d8b7f36a005a8dcd
Liverson Paulo <liverson.p@gmail.com>
2019-11-11 23:13:51 -0300

    New class extending Fruta, this type of fruit make snake have the original size and the player don't lose score with this

--- src/visual/Decrease.java
+++ src/visual/Decrease.java
@@ -1,4 +1,13 @@
 package visual;
 
-public class Decrease {
+import java.awt.*;
+
+public class Decrease extends Fruta {
+    public Decrease(){
+        setCor4(new Color(153,51,255));
+    }
+
+    public int diminuir(){
+        return 5;
+    }
 }
